913bfd1c960f4dc35e451ef081843111465f74d7
Javisebas380 <javisebas380@gmail.com>
2025-11-14 20:33:29 +0100

    Updated LockedSection.tsx

--- components/LockedSection.tsx
+++ components/LockedSection.tsx
@@ -233,7 +233,7 @@ export default function LockedSection({
           <View
             style={[
               StyleSheet.absoluteFillObject,
-              { backgroundColor: 'rgba(13,13,13,0.35)' }
+              { backgroundColor: 'rgba(13,13,13,0.15)' }
             ]}
           />
         </Animated.View>
