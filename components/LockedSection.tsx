7b57bdfc93468ba45e67217741d9df9195b653a5
Javisebas380 <javisebas380@gmail.com>
2025-11-14 20:34:29 +0100

    Updated LockedSection.tsx

--- components/LockedSection.tsx
+++ components/LockedSection.tsx
@@ -219,7 +219,7 @@ export default function LockedSection({
           pointerEvents="none"
         >
           <SafeBlurView
-            intensity={25}
+            intensity={20}
             tint="dark"
             experimentalBlurMethod="dimezisBlurView"
             blurReductionFactor={4}
